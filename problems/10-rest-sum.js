f36206e0dd0f560f66d5ff95d3cfc29cdf129a3e
Nicol Yoshikawa <nicolyoshikawa@Nicols-MBP.attlocal.net>
2022-10-27 21:29:07 -0700

    problem 10

--- problems/10-rest-sum.js
+++ problems/10-rest-sum.js
@@ -11,7 +11,19 @@ restSum(0); // => 0
 
 function restSum(...otherNums) {
   // Your code here
+  let sum = 0;
+  for (let i = 0; i < otherNums.length; i++){
+    let num = otherNums[i];
+    sum += num;
+  }
+
+
+  return sum;
 }
+
+console.log(restSum(3,5,6)); // => 14
+console.log(restSum(1, 2, 3, 4, 5, 6, 7, 8, 9)); // => 45
+console.log(restSum(0)); // => 0
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = restSum;
