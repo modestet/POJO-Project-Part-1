b31fb2c30f51293f88b53b848c055432e5192ed1
Terson Modeste <tersonmodeste@Tersons-MacBook-Air.local>
2022-10-27 21:49:27 -0700

    problem 13

--- problems/13-string-converter.js
+++ problems/13-string-converter.js
@@ -13,7 +13,20 @@ console.log(stringConverter("raccoon")); // => {r: 1, a: 1, c: 2, o: 2, n: 1}
 
 function stringConverter(string) {
   // Your code here
+  let obj = {}
+  for (let i = 0; i < string.length; i++) {
+    let char = string[i]
+    // console.log(char)
+    if (obj[char] !== undefined) {
+      obj[char]++
+    } else {
+      obj[char] = 1
+    }
+  }
+  return obj
 }
-
+console.log(stringConverter("apple")); // => {a: 1, p: 2, l: 1, e: 1}
+console.log(stringConverter("banana")); // => {b: 1, a: 3, n: 2}
+console.log(stringConverter("raccoon")); // => {r: 1, a: 1, c: 2, o: 2, n: 1}
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = stringConverter;
