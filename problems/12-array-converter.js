962daec5e815c95cf7763f2bb10255f84805b752
Nicol Yoshikawa <nicolyoshikawa@Nicols-MBP.attlocal.net>
2022-10-27 21:41:17 -0700

    problem 12

--- problems/12-array-converter.js
+++ problems/12-array-converter.js
@@ -5,14 +5,28 @@ value in the array. **Hint:** don't forget you can check if a key is present
 in an object by using `obj[key] === undefined`.
 
 Examples:
+***********************************************************************/
 
 console.log(arrayConverter(["apple", "apple"])); // => {apple: 2}
 console.log(arrayConverter(["mango", "pineapple"])); // => {mango: 1, pineapple: 1}
 console.log(arrayConverter(["apple", "banana", "potato", "banana"])); // => {apple: 1, banana: 2, potato: 1}
-***********************************************************************/
 
 function arrayConverter(array) {
   // Your code here
+  let obj = {};
+
+  for (let i = 0; i < array.length; i++) {
+    let key = array[i];
+
+    if(obj[key] !== undefined){
+      obj[key]++;
+    } else {
+      obj[key] = 1;
+    }
+  }
+
+  return obj;
+
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
